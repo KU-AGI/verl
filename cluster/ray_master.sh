12531c438c35fb4a8dd91092fae0601ac88df8e4
pimang62 <pimang62@gmail.com>
2025-10-30 10:31:01 +0000

    ray cluster for server 3 & 4

--- cluster/ray_master.sh
+++ cluster/ray_master.sh
@@ -1,16 +1,17 @@
 export MASTER_ADDR=$(hostname -I | awk '{print $1}')
-export MASTER_PORT=6380
-export DASHBOARD_PORT=8266
-export NUM_GPUS=4
+export MASTER_PORT=6379
+export DASHBOARD_PORT=8265
+export NUM_GPUS=8
+export GLOO_SOCKET_IFNAME="bond-srv.1518"
 
 # ray stop --force
 # rm -rf /tmp/ray
 
-RAY_memory_monitor_refresh_ms=0 ray start --head \
+ray start --head \
+  --node-ip-address=$MASTER_ADDR \
   --port=$MASTER_PORT \
   --dashboard-host=0.0.0.0 \
   --dashboard-port=$DASHBOARD_PORT \
   --include-dashboard=True \
   --num-gpus=${NUM_GPUS} \
-  --disable-usage-stats \
-  --temp-dir="/data/ray"
+  --disable-usage-stats
