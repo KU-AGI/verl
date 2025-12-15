ce6b4488b44dfd0494db499daf9ad515cd97ea8b
pimang62 <pimang62@gmail.com>
2025-12-15 11:18:52 +0000

    latest dataset preprocessor

--- examples/data_preprocess/mllm_dataset.py
+++ examples/data_preprocess/mllm_dataset.py
@@ -7,7 +7,7 @@ import datasets
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_dir", default="/data/mllm/janus_r1_sft/data") # flow_grpo/images, reasonr1
-    parser.add_argument("--data_path", default="/data/mllm/flowgrpo_reasonr1_train_format_58891.json")
+    parser.add_argument("--data_path", default="/data/mllm/our_dataset/train/rl_prompt/genevalpp_target_bench_49504.json")
     parser.add_argument("--save_dir", default="/data/mllm/data")
 
     args = parser.parse_args()
@@ -24,12 +24,6 @@ if __name__ == "__main__":
             prompt_id = example["prompt_id"]
             prompt = example["prompt"]
             
-            if "flowgrpo" in prompt_id:
-                root_dir = "/data/mllm/flow_grpo/images"
-            elif "reasonr1" in prompt_id:
-                root_dir = "/data/mllm/reasonr1"
-            aligned_image_path = os.path.join(root_dir, example["aligned_data"]["img_path"].lstrip("/"))
-
             feedback_tuple = example["tuple"]
             vqa_question = example["question"] # for task 2 eval
 
@@ -37,7 +31,7 @@ if __name__ == "__main__":
                 "data_source": "image_generation",
                 "prompt": prompt,
                 "ability": "image_unified_generation",
-                "reward_model": {"style": "rule", "ground_truth": aligned_image_path, "tuple": feedback_tuple, "vqa_question": vqa_question},
+                "reward_model": {"style": "rule", "ground_truth": None, "tuple": feedback_tuple, "vqa_question": vqa_question},
                 "extra_info": {"split": split, "index": idx},
             }
             return data
