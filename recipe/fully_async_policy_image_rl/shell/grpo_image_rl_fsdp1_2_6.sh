6308c5822d23cabdc9dd9fdc33893a6e7410eb5e
pimang62 <pimang62@gmail.com>
2026-03-20 16:33:16 +0000

    chore: exp start shell

--- recipe/fully_async_policy_image_rl/shell/grpo_image_rl_fsdp1_2_6.sh
+++ recipe/fully_async_policy_image_rl/shell/grpo_image_rl_fsdp1_2_6.sh
@@ -14,8 +14,8 @@ exec 2>&1
 #                         EXPERIMENT CONFIGURATION
 ###############################################################################
 project_name='mllm_reasoning'
-# exp_name="0319_our_model_our_dataset_total_step_fine_grained_reward_v2"
-exp_name='testest'
+exp_name="0320_our_model_our_dataset_total_step_fine_grained_reward_replay_buffer_v4_rollout_IS"
+# exp_name='testest'
 task_ids='[1,2,3]'
 
 ###############################################################################
@@ -93,12 +93,12 @@ rollout_mode=async
 use_kl_in_reward=False
 kl_coef=0.0
 use_kl_loss=False
-kl_loss_coef=0.04
+kl_loss_coef=0.001
 
 # PPO Clipping
 clip_ratio_low=0.2
 clip_ratio_high=0.2
-entropy_coeff=0.00
+entropy_coeff=0.0
 
 # Group Filtering
 enable_filter_groups=True
@@ -163,8 +163,8 @@ loss_agg_mode="token-mean"
 ###############################################################################
 lr=5e-6
 lr_scheduler_type=constant
-lr_warmup_steps=0
-weight_decay=0.0
+lr_warmup_steps=10
+weight_decay=0.01
 
 ###############################################################################
 #                        ASYNC TRAINING PARAMETERS
@@ -175,18 +175,26 @@ staleness_threshold=2.0
 trigger_parameter_sync_step=1
 require_batches=2
 partial_rollout=False
-use_rollout_log_probs=False
+use_rollout_log_probs=True
 compute_prox_log_prob=False
 max_regen_retries=3
 
 # Replay Buffer
 replay_buffer_enable=True
 replay_buffer_max_version_gap=-1
-replay_buffer_max_size_per_task=100
+replay_buffer_max_size_per_task=30
 replay_buffer_max_use_count=-1
+replay_buffer_filter_mode=mean      # "mean", "std", or "max"
 replay_buffer_score_threshold_1=0.8
-replay_buffer_score_threshold_2=2.0 # 3점 만점 
+replay_buffer_score_threshold_2=2.0 # 3점 만점
 replay_buffer_score_threshold_3=1.0 # 2점 만점
+
+###############################################################################
+#                        ROLLOUT CORRECTION
+###############################################################################
+rollout_is=token          # null (disabled), "token", "sequence"
+rollout_is_threshold=2.0
+bypass_mode=false
 
 ###############################################################################
 #                         TRAINING SCHEDULE
@@ -239,6 +247,9 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     algorithm.filter_groups.enable=${enable_filter_groups} \
     algorithm.filter_groups.metric=${filter_groups_metric} \
     algorithm.norm_adv_by_std_in_grpo=${norm_adv_by_std_in_grpo} \
+    algorithm.rollout_correction.rollout_is=${rollout_is} \
+    algorithm.rollout_correction.rollout_is_threshold=${rollout_is_threshold} \
+    algorithm.rollout_correction.bypass_mode=${bypass_mode} \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.hybrid_engine=False \
     actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
@@ -291,8 +302,8 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     actor_rollout_ref.ref.fsdp_config.wrap_policy.transformer_layer_cls_to_wrap=['LlamaDecoderLayer'] \
     actor_rollout_ref.ref.ulysses_sequence_parallel_size=${sp_size} \
     trainer.critic_warmup=0 \
-    trainer.logger=['console'] \
-    trainer.val_before_train=False \
+    trainer.logger=['console','wandb'] \
+    trainer.val_before_train=True \
     trainer.balance_batch=False \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${exp_name}" \
@@ -325,6 +336,7 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     async_training.replay_buffer.max_version_gap=${replay_buffer_max_version_gap} \
     async_training.replay_buffer.max_size_per_task=${replay_buffer_max_size_per_task} \
     async_training.replay_buffer.max_use_count=${replay_buffer_max_use_count} \
+    async_training.replay_buffer.filter_mode=${replay_buffer_filter_mode} \
     +async_training.replay_buffer.score_thresholds.1=${replay_buffer_score_threshold_1} \
     +async_training.replay_buffer.score_thresholds.2=${replay_buffer_score_threshold_2} \
     +async_training.replay_buffer.score_thresholds.3=${replay_buffer_score_threshold_3} \
